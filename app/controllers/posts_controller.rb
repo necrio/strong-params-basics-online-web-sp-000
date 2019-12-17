6793d69dedfe4297f574f1db38da1c80b9f8b442
Keith Isham <kisham8899@gmail.com>
2019-12-17 11:36:05 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,13 +23,15 @@ class PostsController < ApplicationController
   redirect_to post_path(@post)
 	end
 	
+		def edit
+	  @post = Post.find(params[:id])
+	end
+	
 	private
  
 def post_params
   params.require(:post).permit(:title, :description)
 end
 
-	def edit
-	  @post = Post.find(params[:id])
-	end
+
 end
